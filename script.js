525722a97149a8fd96ec5a597287c3c195f57acd
Chelsea Okey <chelsea.okey@gmail.com>
2015-05-04 11:33:16 -0500

    First working copy

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-$(document).ready(function(){
-    $('#submit').on('click', function() {
-        $('.appendRow').empty();
+function display(results){
+    console.log("before " + results.html_url);
+    $('.gitLink').attr("href", results.html_url);
+    $('.profile').text(results.html_url);
+    $('.gitAvatar').attr("src", results.avatar_url);
+    $('.gitDescription').text("This user has " + results.public_repos + " repos.");
+    $('repoList').attr("src", results.repos_url);
+    console.log(results.html_url);
+}
+function search(value){
+    $.ajax ({
+        type: 'GET',
+        url: 'http://api.github.com/users/' + encodeURI(value) + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
+        crossDomain: true,
+        dataType: 'json',
+        success: function(response) {
+            console.log(response);
+            display(response);
+        },
+        error: function (xhr, status) {
+            alert('Error: ' + status + '\nThis user may not exist or the name could\'ve been spelled wrong. Try again!');
+        }
+    });
+}
+$(document).ready(function() {
+    $('#submit').on('click', function(e) {
+        $('.gitLink').attr("href", "#");
+        $('.profile').text("");
+        $('.gitDescription').text("");
+        $('.gitAvatar').attr("src", "");
+        $('repoList').attr("src", "#");
+        e.preventDefault();
         var value = $('#searchVal').val();
         search(value);
     });
-    $('.gitLink').attr("href", response.html_url);
-    $('.profile').text(response.html_url);
-    $('.gitDescription').text(response.public_repos);
-    $('.gitAvatar').attr("src", response.imgSrc);
-
-    function search(value){
-        $.ajax ({
-            type: 'GET',
-            url: 'http://api.github.com/users/' + value,
-            crossDomain: true,
-            dataType: 'json',
-            success: function(response) {
-                console.log(response);
-            },
-            error: function (xhr, status) {
-                alert('Error: ' + status);
-            }
-        });
-    }
 });
 
 
